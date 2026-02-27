ca074277371e4bb0925fd4e0e95663f5a777c4d6
lukasako <lukasmoralesarg@gmail.com>
2026-02-27 19:25:15 -0300

    Corrige y deja solo archivos backend

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { AttendanceProvider } from "./context/AttendanceContext.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <AttendanceProvider>
-    <App />
-  </AttendanceProvider>
-);
